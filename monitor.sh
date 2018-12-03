4a03e88bcb7852c9cb9ffe253c2393fb6c20c169
qwqw12100 <qwqw12100@github.com>
2018-12-03 22:05:46 +0000

    setup

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+for value in {0..3}
+do
+	gpio mode $value out
+done
+
+gpio write 0 0
+gpio write 1 0
+gpio write 2 0
+
 first=$(./tempread)
+temp=first
 if [ $first -eq 1 ]; then
 echo "can't read the data"
 exit
@@ -9,20 +20,17 @@ while :
 do
 current=$(./tempread)
 echo "$current"
-let diff=$current-$first
-if [ $diff -gt 1000 -a $diff -lt 2000 ] ; then
-gpio write 0 1
-gpio write 1 0
-gpio write 2 0
+let diff=$current-$temp
+if [ $diff -lt 0 ] ; then
+let diff=$temp-$current
 fi
-if [ $diff -gt 2000 -a $diff -lt 3000 ] ; then
+if [ $diff -gt 1000 ] ; then
 gpio write 0 1
+fi
+if [ $diff -gt 2000 ] ; then
 gpio write 1 1
-gpio write 2 0
 fi
 if [ $diff -gt 3000 ] ; then
-gpio write 0 1
-gpio write 1 1
 gpio write 2 1
 fi
 done
